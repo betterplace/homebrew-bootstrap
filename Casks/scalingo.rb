f388643557cf20059e04790377a71f11f57fdabd
Daniel Hahn <dha@betterplace.org>
2019-04-02 14:59:08 +0200

    Add more helper scripts

--- Casks/scalingo.rb
+++ Casks/scalingo.rb
@@ -1,6 +1,6 @@
 cask 'scalingo' do
-  version '1.9.0'
-  sha256 '56724f083260839c76c0d24f6275221fa6e00d6cec1f597de2d1823b382386b6'
+  version '1.10.1'
+  sha256 'f950ca68af0845e1878b1d00b1a5088434fd5e48842f50acbabe37149dbca17c'
 
   # github.com/Scalingo was verified as official when first introduced to the cask
   url "https://github.com/Scalingo/cli/releases/download/#{version}/scalingo_#{version}_darwin_amd64.zip"
