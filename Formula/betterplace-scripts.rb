0096b3421494ec6611ad895ab3501203e19b894a
Daniel Hahn <dha@betterplace.org>
2019-08-29 17:44:01 +0200

    Try if this is a version

--- Formula/betterplace-scripts.rb
+++ Formula/betterplace-scripts.rb
@@ -2,7 +2,7 @@ class BetterplaceScripts < Formula
   desc "Shell scripts for betterplace.org"
   homepage "https://github.com/betterplace/better-scripts"
   url "https://github.com/betterplace/better-scripts.git",
-    :branch => 'master',
+    :tag => 'v0.0.2',
     :revision => '536a4957921b4187ea3636639b852bdccbab7f5b'
 
   # depends_on "cmake" => :build
