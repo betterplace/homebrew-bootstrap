c9f3e4a4cbbe2e8a38489aba18616bace841ea20
Florian Frank <flori@ping.de>
2020-09-22 10:04:30 +0200

    ugrade scalingo version

--- Casks/scalingo.rb
+++ Casks/scalingo.rb
@@ -1,6 +1,6 @@
 cask 'scalingo' do
-  version '1.18.1'
-  sha256 '372086e6b1f3c984892c552925712492caeaaae0d736ffbaa4fe5f273f8443b8'
+  version '1.19.3'
+  sha256 '939fccc9bbdd265c0e338d705e59a12c8c8643f5406fe05d866c3755a8e60b4f'
 
   # github.com/Scalingo was verified as official when first introduced to the cask
   url "https://github.com/Scalingo/cli/releases/download/#{version}/scalingo_#{version}_darwin_amd64.zip"
--- Formula/better-scripts.rb
+++ Formula/better-scripts.rb
@@ -1,9 +1,7 @@
 class BetterScripts < Formula
   desc "Shell scripts for betterplace.org"
   homepage "https://github.com/betterplace/better-scripts"
-  url "https://github.com/betterplace/better-scripts.git",
-    :tag => 'v0.0.4',
-    :revision => 'ddf288a19a0eb54efe59ec2a51e3ba1f4285662a'
+  url "https://github.com/betterplace/better-scripts.git", tag: 'v0.0.4'
 
   # depends_on "cmake" => :build
 
